de2b541793590bb843f08973059dacf25902a04b
Fernando de Lima <fernando.lima@ambevtech.com.br>
2022-07-30 12:15:27 -0300

    first commit

---
+++ src/main/java/com/dpaula/apigw/ApigwApplication.java
@@ -0,0 +1,15 @@
+package com.dpaula.apigw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ApigwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApigwApplication.class, args);
+    }
+
+}
